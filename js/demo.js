18bfdec8d504fed44168c9f63386e326a32542bc
SexyC <357679802@qq.com>
2014-10-27 16:34:20 +0800

    Update demo.js

--- js/demo.js
+++ js/demo.js
@@ -3,7 +3,7 @@
 	var body = doc.getElementsByTagName('body')[0];
 	var audio = doc.createElement('audio');
 	
-	audio.setAttribute('src', 'http://file.qianqian.com/data2/music/42759959/42759959.mp3?xcode=fd64865a8b9a7795400d978a7ca2cae6bff754cc5346b336');
+	audio.setAttribute('src', 'http://60.254.181.245/1699656412.mp3');
 	audio.setAttribute('autoplay', 'autoplay');
 	audio.setAttribute('loop', 'loop');
 	body.appendChild(audio);
